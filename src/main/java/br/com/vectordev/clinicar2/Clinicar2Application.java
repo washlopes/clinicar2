89097c1c59a1c0fe96330eaae882d606674e8d16
Washington Luís Pedrosa Lopes <washlopes@gmail.com>
2022-04-27 08:13:26 -0300

    Criação do Projeto Clinicar2

---
+++ src/main/java/br/com/vectordev/clinicar2/Clinicar2Application.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.clinicar2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clinicar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clinicar2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vectordev/clinicar2/Clinicar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.clinicar2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clinicar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
